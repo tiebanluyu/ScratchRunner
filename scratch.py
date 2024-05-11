5e9b234086876c99ec13a425f11027280eaef848
John Doe <johndoe@example.com>
2024-05-11 17:53:09 +0800

    17.53

--- scratch.py
+++ scratch.py
@@ -1,6 +1,4 @@
-import turtle
-
-import json
+import json ,pygame
 t=json.loads(open("project.json","r",encoding="utf-8").read())
 
 class Sprite:
@@ -8,11 +6,7 @@ class Sprite:
 
         self.__dict__.update(dict1)
         #print(self.name)
-    def draw(self):
-        t=turtle.Turtle()
-        turtle.register_shape("1.gif")
 
-        t.shape("1.gif")
 
     
 list1=[]
@@ -20,11 +14,43 @@ for i in t["targets"]:
     o=Sprite(i)
     list1.append(o)
     print(o,o.name)
-turtle.setworldcoordinates(-240, -180, 240, 180)
-turtle.tracer(False)
 
-while 1:
-    turtle.clear()
-    for sprite in list1:
-        sprite.draw()
-    turtle.update()
+
+# 初始化Pygame
+pygame.init()
+
+# 设置窗口大小
+size = (480, 360)
+position = (0,0)
+
+screen = pygame.display.set_mode(size)
+
+# 设置窗口标题
+pygame.display.set_caption("My Game")
+
+# 设置矩形位置和大小
+rect_x = 50
+rect_y = 50
+rect_width = 50
+rect_height = 50
+
+# 设置颜色
+red = (255, 0, 0)
+
+# 游戏循环
+done = False
+while not done:
+    # 处理事件
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+
+    # 填充窗口颜色
+    screen.fill((255, 255, 255))
+
+    pygame.blit("costume1.svg")
+
+    # 更新窗口
+    pygame.display.update()
+
+# 退出Pygame 
