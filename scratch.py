64826d0c9cc5c33e368f84fe95ebb67876e0543f
John Doe <johndoe@example.com>
2024-05-11 22:50:43 +0800

    22.50

--- scratch.py
+++ scratch.py
@@ -1,6 +1,17 @@
-import json ,pygame
-def positionmap(x,y):
-    return 240-x,180-y
+import json 
+import pygame # type: ignore
+# 设置窗口大小
+size = (480, 360)
+position = (0,0)
+origin_x = size[0] // 2
+origin_y = size[1] // 2
+ 
+# 自定义坐标转换函数
+def positionmap(x:int, y):
+    new_x = x + origin_x
+    new_y = -y + origin_y
+    return new_x, new_y
+
 
 t=json.loads(open("project.json","r",encoding="utf-8").read())
 
@@ -10,9 +21,20 @@ class Sprite():
         self.__dict__.update(dict1)
         #print(self.name)
     def draw(self):
-        image = pygame.image.load(self.costumes[0]["md5ext"])
-        screen.blit(image, (self.x, self.y))
+        image = pygame.image.load(self.costumes[self.currentCostume]["md5ext"])
+        screen.blit(image, positionmap(self.x, self.y))
+    codes={"forward":lambda self,x:exec("self.x+=x")}    
+def runcode(sprite,flag):
+    code=sprite.block[flag]
+    if code["opcode"]=="motion_turnright":
+        self.angle+=###########################################
+def run(sprite):
+    for flag,code in sprite.block.items():
+        if code["opcode"]=="event_whenflagclicked":
+            flag=code[next]
+            runcode(sprite,flag)
 
+            
     
 list1=[]
 for i in t["targets"]:
@@ -40,6 +62,7 @@ done = False
 
 while not done:
     # 处理事件
+    list1[1].x+=__import__("random").random()*0.01
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
